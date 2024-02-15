ef11f45997ac4e51807dbe1880a243832039deff
Mirna Ashour <mirna.ashour@gmail.com>
2024-02-15 14:37:06 -0500

    Created form for adding a new journal

--- src/Components/Journals/Journals.jsx
+++ src/Components/Journals/Journals.jsx
@@ -1,30 +1,95 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
+function getCurrentTimestamp() {
+    const currentDate = new Date();
+
+    // Get date components
+    const year = currentDate.getFullYear();
+    const month = String(currentDate.getMonth() + 1).padStart(2, '0'); // Months are zero-indexed, so we add 1
+    const day = String(currentDate.getDate()).padStart(2, '0');
+    const hours = String(currentDate.getHours()).padStart(2, '0');
+    const minutes = String(currentDate.getMinutes()).padStart(2, '0');
+    const seconds = String(currentDate.getSeconds()).padStart(2, '0');
+
+    // Assemble formatted date string
+    const formattedDate = `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
+
+    return formattedDate;
+}
+
+
+function AddJournal({ setError, fetchJournals }) {
+	const [title, setTitle] = useState('');
+	const [content, setContent] =useState('');
+
+	const changeTitle = (event) => { setTitle(event.target.value); };
+	const changeContent = (event) => {setContent(event.target.value); };
+
+	const addJournal = (event) => {
+		event.preventDefault();
+		axios.post('http://localhost:8000/journals', { title: title, content: content, timestamp: getCurrentTimestamp(), modified: getCurrentTimestamp() }) // actual attribute name: this file's var/val
+			.then(() => {
+				setError('');
+				fetchJournals();
+			})
+			.catch((e) => {
+			    if (e.response && e.response.data && e.response.data.message) {
+			        setError(e.response.data.message);
+			    } else {
+					if (e.response && e.response.data && e.response.data.message) {
+						setError(e.response.data.message);
+					} else {
+						setError('There was a problem adding the journal entry');
+					}
+			    }
+			});
+	};
+
+	return (
+		<form>
+			<label htmlFor="title">
+				Journal Title
+			</label>
+			<input type="text" id="title" value={title} onChange={changeTitle} />
+
+			<label htmlFor="user">
+				Content
+			</label>
+			<input type="text" id="content" value={content} onChange={changeContent} />
+
+			<button type="submit" onClick={addJournal}>Submit</button>
+		</form>
+	);
+}
+
+
 function Journals() {
 
 	const [error, setError] = useState('');
 	const [journals, setJournals] = useState([]);
 
+	const fetchJournals = () => {
+		axios.get('http://localhost:8000/journals')
+			.then((response) => {
+				const journalsObject = response.data.Data;
+				const keys = Object.keys(journalsObject);
+				const journalsArray = keys.map((key) => journalsObject[key]);
+				setJournals(journalsArray);
+			}) // something good
+			.catch((e) => {
+				if (e.response && e.response.data && e.response.data.message) {
+					setError(e.response.data.message);
+				} else {
+					setError('There was a problem fetching all journals.');
+				}
+			}); // something bad
+	};
+	
 	useEffect(
-		() => {
-			axios.get('http://localhost:8000/journals')
-				.then((response) => {
-					const journalsObject = response.data.Data;
-					const keys = Object.keys(journalsObject);
-					const journalsArray = keys.map((key) => journalsObject[key]);
-					setJournals(journalsArray);
-				}) // something good
-				.catch((e) => {
-					if (e.response && e.response.data && e.response.data.message) {
-						setError(e.response.data.message);
-					} else {
-						setError('There was a problem fetching all journals.');
-					}
-				}); // something bad
-		},
+		fetchJournals,
 		[],
-	);
+	)
 
 	return (
 		<div className="wrapper">
@@ -37,6 +102,8 @@ function Journals() {
 					{error}
 				</div>
 			)}
+
+			<AddJournal setError={setError} fetchJournals={fetchJournals} />
 
 			{journals.map((journal) => (
 				<div className="category-container">
