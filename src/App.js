d5e13c98f885f45568c44075492f20a45fd4175b
Cody Jiang <zj2247@nyu.edu>
2024-03-24 22:27:38 -0400

    Updated the animation of Login Page + Added LOGO

--- src/App.js
+++ src/App.js
@@ -1,22 +1,21 @@
-import {
-  BrowserRouter,
-  Routes,
-  Route,
-} from 'react-router-dom';
-
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
-
 import Navbar from './Components/Navbar';
 import Categories from './Components/Categories';
 import Users from './Components/Users';
 import Journals from './Components/Journals';
 import Login from './Components/Login';
 import Register from './Components/Register';
+import Logo from './EaseJournalLOGO_V1_vector.png'; // Import the logo
 
 function App() {
   return (
     <BrowserRouter>
       <Navbar/>
+      <div className="logo-container" style={{ textAlign: 'center', margin: '2rem 0' }}>
+        <img src={Logo} alt="Ease Journal Logo" style={{ maxWidth: '200px' }} />
+      </div>
       <Routes>
         <Route path="" element={ <Login/> } />
         <Route path="categories" element={ <Categories/> } />
