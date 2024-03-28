5e340397af69a7dc9d3714baed8ba2589e395c99
Mahmoud1813 <mga8876@nyu.edu>
2024-03-28 11:20:32 -0400

    Added import in Navbar.jsx to fix react error

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 
 const PAGES = [
   { label: 'Home', destination: '/' },
