6f5b0d1933f1603ded6675676999b65be65771d1
Mahmoud1813 <mga8876@nyu.edu>
2024-04-11 13:04:09 -0400

    Attempted to get the form from the backend but having problems

--- src/Components/Register/Register.jsx
+++ src/Components/Register/Register.jsx
@@ -1,74 +1,28 @@
 import React, { useState, useEffect } from 'react';
-import propTypes from 'prop-types';
+import PropTypes from 'prop-types';
 import axios from 'axios';
 import { BACKEND_URL } from '../../constants';
 
 const USERS_ENDPOINT = `${BACKEND_URL}/users`;
 const SIGNUP_FORM_ENDPOINT = `${BACKEND_URL}/signup/form`;
 
-const FORM = [
-  {
-    FLD_NM: 'first_name',
-    type: 'text',
-  },
-
-  {
-    FLD_NM: 'last_name',
-    type: 'text',
-  },
-
-  {
-    FLD_NM: 'dob', 
-    type: 'date',
-  },
-
-  {
-    FLD_NM: 'email',
-    type: 'text',
-  },
-
-  {
-    FLD_NM: 'password',
-    type: 'password',
-  },
-];
-
-/* This function just takes an array 'fields' (of the format described above) and
- * creates an object where the keys are the fieldName values for each item in 'fields'
- * and the values are the values given by the user. If you don't set an initial value
- * (here it's an empty string ''), when the user enters a value you'll switch from
- * an uncontrolled input (user can do whatever) to a controlled input (value is fixed).
- * Usually, if you're writing a frontend in React, you want all inputs to be
- * controlled.
- */
 function fieldsToAnswers(fields) {
-    const answers = {};
-    fields.forEach(({ FLD_NM }) => { answers[FLD_NM] = ''; });
-    return answers;
-  }
-  
+  const answers = {};
+  fields.forEach(({ FLD_NM }) => {
+    answers[FLD_NM] = '';
+  });
+  return answers;
+}
+
 function Form({ fields }) {
   const [answers, setAnswers] = useState(fieldsToAnswers(fields));
-
-  useEffect(() => {
-    async function fetchForm() {
-      try {
-        const response = await axios.get(SIGNUP_FORM_ENDPOINT);
-        const formFields = response.data;
-        setAnswers(fieldsToAnswers(formFields));
-      } catch (error) {
-        console.error('Error fetching form:', error);
-      }
-    }
-    fetchForm();
-  }, []);
 
   const answerQuestion = (fieldName, value) => {
     setAnswers({ ...answers, [fieldName]: value });
   };
 
   const handleSubmit = async (e) => {
-    // e.preventDefault();
+    e.preventDefault();
     try {
       await axios.post(USERS_ENDPOINT, answers);
       alert('Registered successfully!');
@@ -86,7 +40,9 @@ function Form({ fields }) {
             id={FLD_NM}
             type={type}
             value={answers[FLD_NM]}
-            onChange={(e) => { answerQuestion(FLD_NM, e.target.value); }}
+            onChange={(e) => {
+              answerQuestion(FLD_NM, e.target.value);
+            }}
           />
         </div>
       ))}
@@ -96,12 +52,30 @@ function Form({ fields }) {
 }
 
 Form.propTypes = {
-  fields: propTypes.arrayOf(propTypes.shape({
-    FLD_NM: propTypes.string,
-    type: propTypes.string,
-  })).isRequired,
+  fields: PropTypes.arrayOf(
+    PropTypes.shape({
+      FLD_NM: PropTypes.string,
+      type: PropTypes.string,
+    })
+  ).isRequired,
 };
 
 export default function FormWrapper() {
-  return <Form fields={FORM} />;
+  const [formFields, setFormFields] = useState([]);
+
+  useEffect(() => {
+    async function fetchForm() {
+      try {
+        const response = await axios.get(SIGNUP_FORM_ENDPOINT);
+        const formFieldsData = response.data;
+        setFormFields(formFieldsData);
+      } catch (error) {
+        console.error('Error fetching form:', error);
+      }
+    }
+
+    fetchForm();
+  }, []);
+
+  return <Form fields={formFields} />;
 }
